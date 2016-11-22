f721abfb1ac878ebd7a78aef5023fb3cf863ce01
Kang Sung Soo <tjdtn.0330@gmail.com>
2016-11-22 13:53:11 +0900

    layout

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 201611211844) do
+ActiveRecord::Schema.define(version: 20161122042105) do
+
+  create_table "microposts", force: :cascade do |t|
+    t.text     "content"
+    t.integer  "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "name"
+    t.string   "email"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "whyers", force: :cascade do |t|
     t.string   "name"
